72ee0e4e100d3e4de4eefa8bb2157317a5239364
Katherine Abela <katherineabela@mail.weber.edu>
2025-04-24 02:07:46 -0400

    bad gateway error

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 from app import create_app, db
 from flask_migrate import Migrate
-from livereload import Server
 from dotenv import load_dotenv
 import os
 
 load_dotenv(override=True)
 
 app = create_app()
-
 migrate = Migrate(app, db)
 
-# Dev server with live reload
-if __name__ == "__main__":
-    if os.getenv("FLASK_ENV") == "development":
-        server = Server(app.wsgi_app)
-        server.watch("app/**/*.py")
-        server.watch("app/templates/**/*.html")
-        server.watch("app/static/**/*.css")
-        server.serve(port=5000)
-    else:
-        # app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
-        app.run(port=5000)
+# Use Livereload only in development
+if os.getenv("FLASK_ENV") == "development":
+    from livereload import Server
+    server = Server(app.wsgi_app)
+    server.watch("app/**/*.py")
+    server.watch("app/templates/**/*.html")
+    server.watch("app/static/**/*.css")
+    server.serve(port=5000)
+
