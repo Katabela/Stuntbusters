12de19bf5aa8f7a724f5d6db20644d23e86e8469
Katherine Abela <katherineabela@mail.weber.edu>
2025-04-24 03:06:43 -0400

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -12,8 +12,18 @@ csrf = CSRFProtect()
 def create_app(environment="production"):
     app = Flask(__name__, instance_relative_config=True)
 
-    # Load configurations based on the environment
-    app.config.from_object(Config)
+     # Base Config from .env
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev-secret")
+    app.config["DEBUG"] = environment == "development"
+    app.config["DOMAIN_URL"] = os.getenv("DOMAIN_URL")
+
+    # Gmail API Credentials
+    app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+    app.config["GMAIL_CLIENT_ID"] = os.getenv("GMAIL_CLIENT_ID")
+    app.config["GMAIL_CLIENT_SECRET"] = os.getenv("GMAIL_CLIENT_SECRET")
+    app.config["GMAIL_REFRESH_TOKEN"] = os.getenv("GMAIL_REFRESH_TOKEN")
 
     db.init_app(app)
     mail.init_app(app)
